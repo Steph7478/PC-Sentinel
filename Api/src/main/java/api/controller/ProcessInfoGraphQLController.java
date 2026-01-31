c7e940bccc20f68e22b9cc33ebaf5075615474c4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-31 11:52:38 -0300

    refactor: using autowired

--- Api/src/main/java/api/controller/ProcessInfoGraphQLController.java
+++ Api/src/main/java/api/controller/ProcessInfoGraphQLController.java
@@ -2,6 +2,8 @@ package api.controller;
 
 import api.dto.ProcessInfoDTO;
 import api.kafka.consumer.GraphQLInfoConsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -9,11 +11,8 @@ import reactor.core.publisher.Mono;
 @Controller
 public class ProcessInfoGraphQLController {
 
-    private final GraphQLInfoConsumer infoConsumer;
-
-    public ProcessInfoGraphQLController(GraphQLInfoConsumer infoConsumer) {
-        this.infoConsumer = infoConsumer;
-    }
+    @Autowired
+    private GraphQLInfoConsumer infoConsumer;
 
     @QueryMapping
     public Mono<ProcessInfoDTO> processInfo() {
--- Api/src/main/java/api/controller/UsageGraphQLController.java
+++ Api/src/main/java/api/controller/UsageGraphQLController.java
@@ -1,6 +1,7 @@
 package api.controller;
 
 import api.dto.MetricDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import api.kafka.consumer.GraphQLUsageConsumer;
 import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,8 @@ import reactor.core.publisher.Flux;
 @Controller
 public class UsageGraphQLController {
 
-    private final GraphQLUsageConsumer usageConsumer;
-
-    public UsageGraphQLController(GraphQLUsageConsumer usageConsumer) {
-        this.usageConsumer = usageConsumer;
-    }
+    @Autowired
+    private GraphQLUsageConsumer usageConsumer;
 
     @SubscriptionMapping
     public Flux<MetricDTO> streamUsage() {
